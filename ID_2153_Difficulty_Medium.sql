2e0b3b25c2a8128b70a66982ccc2db0cbad51367
Ganapathy Raaman Balaji <83726546+ganapathy-r-balaji@users.noreply.github.com>
2023-09-02 01:57:02 -0500

    ID_2153_Difficulty_Medium.sql

---
+++ ID_2153_Difficulty_Medium.sql
@@ -0,0 +1,26 @@
+'''
+	Workers With The Highest And Lowest Salaries
+
+	ID: 10152
+
+	PROBLEM STATEMENT:
+	You have been asked to find the employees with the highest and lowest salary. 
+	Your output should include the employee ID, salary, and department, as well as a column salary_type that categorizes the output by:
+		'Highest Salary' represents the highest salary
+		'Lowest Salary' represents the lowest salary.
+
+	DATA DICTIONARY:
+
+	Table: worker
+	worker_id: int
+	first_name: varchar
+	last_name: varchar
+	salary: int
+	joining_date: datetime
+	department: varchar
+
+	Table: title
+	worker_ref_id: int
+	worker_title: varchar
+	affected_from: datetime
+'''
