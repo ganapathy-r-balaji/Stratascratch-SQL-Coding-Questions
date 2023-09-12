5027da8966fa3a10be3610af18a6e8f69f0b9a0c
Ganapathy Raaman Balaji <83726546+ganapathy-r-balaji@users.noreply.github.com>
2023-09-12 17:01:46 -0500

    Create ID_2017_Difficulty_Easy.sql

---
+++ ID_2017_Difficulty_Easy.sql
@@ -0,0 +1,42 @@
+'''
+	Paid Users In April 2020
+	ID 2017
+
+	PROBLEM STATEMENT:
+		How many paid users had any calls in Apr 2020?
+	
+	Table: rc_calls					Table: rc_users				
+	user_id: int					  user_id: int							
+	date: datetime          status: varchar				
+	call_id: int            company_id: int			
+	
+	First 5 rows of the "rc_calls" table:
+	---------------------------------------------
+	user_id		date			call_id
+	1218		4/19/2020 1:06		0
+	1554		3/1/2020 16:51		1
+	1857		3/29/2020 7:06		2
+	1525		3/7/2020 2:01		  3
+	1271		4/28/2020 21:39		4
+
+	Rows of the "rc_users" table:
+	---------------------------
+	user_id		status		company_id
+	1218		free		    1
+	1554		inactive	  1
+	1857		free		    2
+	1525		paid		    1
+	1271		inactive	  2
+
+'''
+
+SELECT 
+    user_id
+FROM rc_users
+WHERE status = 'paid'
+AND user_id IN (
+    SELECT 
+        user_id
+    FROM rc_calls
+    WHERE EXTRACT(month FROM date)=4
+)
