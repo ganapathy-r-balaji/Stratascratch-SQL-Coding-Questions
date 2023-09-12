7e863496fdb0b7e6b26d1e227419826f598dada9
Ganapathy Raaman Balaji <83726546+ganapathy-r-balaji@users.noreply.github.com>
2023-09-12 17:09:16 -0500

    Create ID_2021_Difficulty_Medium.sql

---
+++ ID_2021_Difficulty_Medium.sql
@@ -0,0 +1,34 @@
+'''
+	Initial Call Duration
+	ID 2021
+
+	PROBLEM STATEMENT:
+		Redfin helps clients to find agents. Each client will have a unique request_id and each request_id has several calls. 
+		For each request_id, the first call is an “initial call” and all the following calls are “update calls”.  What is the average call duration for all initial calls?
+
+	Table: redfin_call_tracking
+	Data Dictionary
+		created_on: datetime
+		request_id: int
+		call_duration: int
+		id: int
+
+	First 5 rows of the table:
+	---------------------------
+	created_on		request_id		call_duration		id
+	3/1/2020 4:08	2				3					1
+	3/1/2020 5:28	1				28					2
+	3/1/2020 7:27	2				22					3
+	3/1/2020 13:18	1				12					4
+	3/1/2020 15:08	2				13					5
+'''
+
+SELECT
+    AVG(call_duration) AS avg_call_duration
+FROM (
+    SELECT 
+        call_duration,
+        ROW_NUMBER() OVER(PARTITION BY request_id ORDER BY created_on) AS row_num
+    FROM redfin_call_tracking
+) a
+WHERE row_num = 1
