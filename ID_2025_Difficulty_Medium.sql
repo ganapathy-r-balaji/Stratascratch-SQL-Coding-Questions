848a1b14decffd2013ff0d623612e98dd7214bc8
Ganapathy Raaman Balaji <83726546+ganapathy-r-balaji@users.noreply.github.com>
2023-09-13 09:33:41 -0500

    Update ID_2025_Difficulty_Medium.sql

--- ID_2025_Difficulty_Medium.sql
+++ ID_2025_Difficulty_Medium.sql
@@ -17,7 +17,7 @@
 	
 	First 5 rows of the table:
 	---------------------------
-	id	time_id		user_id		customer_id	client_id	event_type				event_id
+	id	time_id		user_id		customer_id	client_id	event_type			event_id
 	1	2/28/2020	3668-QPYBK	Sendit		desktop		message sent			3
 	2	2/28/2020	7892-POOKP	Connectix	mobile		file received			2
 	3	4/3/2020	9763-GRSKD	Zoomit		desktop		video call received		7
