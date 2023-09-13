9b5f49cc10bc1e1613a7810313bf984d6591c180
Ganapathy Raaman Balaji <83726546+ganapathy-r-balaji@users.noreply.github.com>
2023-09-13 09:33:14 -0500

    ID_2025_Difficulty_Medium.sql

---
+++ ID_2025_Difficulty_Medium.sql
@@ -0,0 +1,40 @@
+'''
+	Users Exclusive Per Client
+	ID 2025
+
+	PROBLEM STATEMENT:
+		Write a query that returns a number of users who are exclusive to only one client. Output the client_id and number of exclusive users.
+
+	Table: postmates_orders
+	Data Dictionary
+		id: int
+		time_id: datetime
+		user_id: varchar
+		customer_id: varchar
+		client_id: varchar
+		event_type: varchar
+		event_id: int
+	
+	First 5 rows of the table:
+	---------------------------
+	id	time_id		user_id		customer_id	client_id	event_type				event_id
+	1	2/28/2020	3668-QPYBK	Sendit		desktop		message sent			3
+	2	2/28/2020	7892-POOKP	Connectix	mobile		file received			2
+	3	4/3/2020	9763-GRSKD	Zoomit		desktop		video call received		7
+	4	4/2/2020	9763-GRSKD	Connectix	desktop		video call received		7
+	5	2/6/2020	9237-HQITU	Sendit		desktop		video call received		7
+'''
+
+SELECT
+    client_id,
+    COUNT(DISTINCT user_id) AS user_count
+FROM fact_events
+WHERE user_id IN (
+    SELECT 
+        user_id
+    FROM fact_events
+    GROUP BY user_id
+    HAVING COUNT(DISTINCT client_id) = 1
+)
+GROUP BY client_id
+
