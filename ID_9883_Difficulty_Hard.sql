327f35b9222fe8337360c77e52d645365ad01b5f
Ganapathy Raaman Balaji <83726546+ganapathy-r-balaji@users.noreply.github.com>
2023-10-13 15:30:43 -0500

    Update ID_9883_Difficulty_Hard.sql

--- ID_9883_Difficulty_Hard.sql
+++ ID_9883_Difficulty_Hard.sql
@@ -1,27 +1,27 @@
 '''
-	Find the oldest survivor per passenger class
-	ID 9883
+Find the oldest survivor per passenger class
+ID 9883
 
-	PROBLEM STATEMENT:
-		Find the oldest survivor of each passenger class.
-		Output the name and the age of the survivor along with the corresponding passenger class.
-		Order records by passenger class in ascending order.
+PROBLEM STATEMENT:
+	Find the oldest survivor of each passenger class.
+	Output the name and the age of the survivor along with the corresponding passenger class.
+	Order records by passenger class in ascending order.
 
 
   Table: lyft_rides
   Data Dictionary
-		passengerid: int
-		survived: int
-		pclass: int
-		name: varchar
-		sex: varchar
-		age: float
-		sibsp: int
-		parch: int
-		ticket: varchar
-		fare: float
-		cabin: varchar
-		embarked: varchar
+	passengerid: int
+	survived: int
+	pclass: int
+	name: varchar
+	sex: varchar
+	age: float
+	sibsp: int
+	parch: int
+	ticket: varchar
+	fare: float
+	cabin: varchar
+	embarked: varchar
 '''
 
 SELECT
